8154d8b50f2ea5b237c283e927fd5718fb975020
niunay <agp119@ic.ac.uk>
2021-10-25 16:05:19 +0100

    Making conflict

--- src/main/java/Drawing.java
+++ src/main/java/Drawing.java
@@ -21,7 +21,7 @@ public class Drawing extends Canvas {
     }
 
     private void setupShapes() {
-        circle=new Circle(new Point(200,200),new Color(0x992267),80);
+        circle=new Circle(new Point(200,200),new Color(0x992266),80);
         rect=new Rect(new Point(100,100),new Color(0x922266),80, 30);
         sqr=new Square(new Point(200,100),new Color(0x552266),80);
     }
